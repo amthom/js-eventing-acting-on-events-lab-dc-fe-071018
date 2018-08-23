55d105874802d6e62f04c695b831b2ec0fa88768
Amanda Thomas <amandamariathomas@gmail.com>
2018-08-23 23:46:54 +0000

    automatically backed up by learn

--- index.js
+++ index.js
@@ -1,7 +1,7 @@
 
 var dodger = document.getElementById('dodger')
 
-dodger.style.backgroundColor = "pink"
+dodger.style.backgroundColor = "magenta"
 dodger.style.left // "180px"
 dodger.style.bottom // "0px"
 
